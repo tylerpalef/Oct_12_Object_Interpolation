cf2e505a2ba27e89af70521b5138b4655abe0814
“Tyler <tylerpalef@gmail.com>
2018-10-12 12:50:05 -0400

    started working on a new method that will reverse the same string without calling .reverse

--- string_reversal.rb
+++ string_reversal.rb
@@ -2,5 +2,10 @@ def reverse_string(str)
   str.reverse
 end
 
+def reverse_string_without_reverse(str)
+  new_word = [] # making a new array to hold the new word
+end
 
 puts reverse_string('this is a test')
+
+puts "-----------"
