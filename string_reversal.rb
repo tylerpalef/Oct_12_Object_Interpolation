04d8c6ff8fee1d578573076957eef7d81952e474
“Tyler <tylerpalef@gmail.com>
2018-10-12 12:53:02 -0400

    successfully created an iteration over the string. Need to reverse the order of the letters now

--- string_reversal.rb
+++ string_reversal.rb
@@ -4,8 +4,15 @@ end
 
 def reverse_string_without_reverse(str)
   new_word = [] # making a new array to hold the new word
+
+  str.split.each do |letter| # Splitting the string into an array
+    new_word << letter
+  end
+  new_word.join(" ") # This is to combine the array with spaces between each word
 end
 
 puts reverse_string('this is a test')
 
 puts "-----------"
+
+puts reverse_string_without_reverse('this is a test')
