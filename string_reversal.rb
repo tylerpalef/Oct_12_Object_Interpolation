088d11ae9e2830f9c2ef970801ebbb2218df97cf
“Tyler <tylerpalef@gmail.com>
2018-10-12 13:07:26 -0400

    attempted to follow another article to see how they reversed the string

--- string_reversal.rb
+++ string_reversal.rb
@@ -3,17 +3,24 @@ def reverse_string(str)
 end
 
 def reverse_string_without_reverse(str)
-  new_word = [] # making a new array to hold the new word
-  counter = 0
-  
-  str.split.each do |letter| # Splitting the string into an array
-    new_word << letter
-    letter.each do |swap| # This line is erroing out
-      letter[swap] = letter[swap+1]
-    end
-  end
-  new_word.join(" ") # This is to combine the array with spaces between each word
+  split_str = str.split
+  reversed = [] # Need this to push/pop items into the array
+  str.size.times { reversed << split_str.pop}
+  # times is to tell the method to run the amount of times the string exists
 end
+
+
+#   new_word = [] # making a new array to hold the new word
+#   counter = 0
+#
+#   str.split.each do |letter| # Splitting the string into an array
+#     new_word << letter
+#     new_word.each do |swap| # This line is erroing out
+#       # Code goes here with the counter
+#     end
+#   end
+#   new_word.join(" ") # This is to combine the array with spaces between each word
+# end
 
 puts reverse_string('this is a test')
 
