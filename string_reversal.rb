b664ea8805fbb70c04a19a5252e137dfa464da5a
“Tyler <tylerpalef@gmail.com>
2018-10-12 13:10:25 -0400

    saving the completed code on reversing the string. I was missing the size.times.do to tell the method how many times to run

--- string_reversal.rb
+++ string_reversal.rb
@@ -3,24 +3,25 @@ def reverse_string(str)
 end
 
 def reverse_string_without_reverse(str)
-  split_str = str.split
+  split_str = str.split('') # This is to add a space after each word is split up
   reversed = [] # Need this to push/pop items into the array
   str.size.times { reversed << split_str.pop}
+  reversed.join
   # times is to tell the method to run the amount of times the string exists
 end
 
-
-#   new_word = [] # making a new array to hold the new word
-#   counter = 0
-#
-#   str.split.each do |letter| # Splitting the string into an array
-#     new_word << letter
-#     new_word.each do |swap| # This line is erroing out
-#       # Code goes here with the counter
-#     end
-#   end
-#   new_word.join(" ") # This is to combine the array with spaces between each word
-# end
+# Original attempt
+  #   new_word = [] # making a new array to hold the new word
+  #   counter = 0
+  #
+  #   str.split.each do |letter| # Splitting the string into an array
+  #     new_word << letter
+  #     new_word.each do |swap| # This line is erroing out
+  #       # Code goes here with the counter
+  #     end
+  #   end
+  #   new_word.join(" ") # This is to combine the array with spaces between each word
+  # end
 
 puts reverse_string('this is a test')
 
